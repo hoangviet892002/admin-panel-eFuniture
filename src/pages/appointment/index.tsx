c6c7aa5c32cdac6107c14790e0f58b1e19653bda
viet <viethxse@gmail.com>
2024-03-15 20:48:08 +0700

    biuld auto login

--- src/pages/appointment/index.tsx
+++ src/pages/appointment/index.tsx
@@ -16,9 +16,9 @@ const AppointmentPage = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
   const columns = [
-    { id: "nameCustomer", label: "Khách hàng ", minWidth: 150 },
+    { id: "customerName", label: "Khách hàng ", minWidth: 150 },
     {
-      id: "nameStaff",
+      id: "staffName",
       label: "Nhân viên",
       minWidth: 150,
     },
@@ -26,7 +26,7 @@ const AppointmentPage = () => {
     { id: "time", label: "Thời gian", minWidth: 100 },
     { id: "email", label: "Email", minWidth: 100 },
     {
-      id: "phone",
+      id: "phoneNumber",
       label: "Số điện thoại",
       minWidth: 100,
     },
@@ -46,30 +46,21 @@ const AppointmentPage = () => {
       const response = await AppointmentService.getAppointmentsByPage(
         currentPage
       );
-      setAppointments(response);
+      setAppointments(response.items);
     } catch (error) {
       console.error("Failed to fetch vouchers");
     }
   };
 
-  const fetchTotalPages = async () => {
-    try {
-      const response = await AppointmentService.getTotalPages(searchTerm, date);
-      setTotalPages(response);
-    } catch (error) {
-      console.error("Failed to fetch total pages", error);
-    }
-  };
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(0);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [date, setDate] = useState("");
+
   useEffect(() => {
     setLoading(true);
-    fetchTotalPages();
+
     fetchAppointments();
     setLoading(false);
-  }, [currentPage, searchTerm, date]);
+  }, [currentPage]);
   const [appointments, setAppointments] = useState<Appointment[]>([]);
   const onViewDetails = (id: string) => {
     navigate(`${id}`);
@@ -77,12 +68,7 @@ const AppointmentPage = () => {
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
   };
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
-  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDate(event.target.value);
-  };
+
   return (
     <Box sx={{ display: "flex" }}>
       <SidebarMenu />
@@ -90,20 +76,7 @@ const AppointmentPage = () => {
         <Typography variant="h4" gutterBottom>
           Danh Sách Buổi hẹn
         </Typography>
-        <TextField
-          label="ngày"
-          type="date"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          style={{ marginRight: 20 }}
-          onChange={handleDateChange}
-        />
-        <TextField
-          label="Tìm kiếm theo tên khách hàng"
-          variant="outlined"
-          onChange={handleSearchChange}
-        />
+
         {loading ? (
           <Loading />
         ) : (
